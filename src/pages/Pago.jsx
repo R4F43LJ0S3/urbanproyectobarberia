2aa35d4960d4ef8b898a78129d1db42963689ace
R4F43LJ0S3 <rafaeljosebarrigamejia2005@gmail.com>
2025-10-29 23:20:12 -0500

    actualizado

--- src/pages/Pago.jsx
+++ src/pages/Pago.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import "../styles/Pago.css";
 
 const Pago = () => {
   const navigate = useNavigate();
